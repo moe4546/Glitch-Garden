c885f9c8ac83980c7d0de17724279dbc8a8e2a47
moe4546 <horn.moritz@web.de>
2019-02-22 19:08:17 +0100

    Splash and Start Screen

---
+++ Glitch Garden/Glitch Garden/Assets/_Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float splashWaitTime = 3f;
+
+    int currentSceneIndex;
+
+    void Awake()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitTime(splashWaitTime));
+        } 
+    }
+
+    IEnumerator WaitTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
